11f6775ad894c6d85203982a24d48e008695d48c
박민성 <102217591+minsung060607@users.noreply.github.com>
2023-02-09 10:36:56 +0900

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,11 @@
+//책으로 쓰는 타입 - documentation
+
+struct Book;
+
 fn main() {
     println!("Hello, world!");
+    let _x/*: i16 */ = 10;
+    //_를 넣어서 시작하면 컴파일러가 무시함
+    //한국어로 변수 이름 선언 가능
+    // 이게 코멘트 입니다
 }
