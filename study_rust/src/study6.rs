c0720d47b4c07d168c1fb232dd00d30d5b13cf91
박민성 <102217591+minsung060607@users.noreply.github.com>
2023-02-16 23:10:25 +0900

    Add files via upload

---
+++ study_rust/src/study6.rs
@@ -0,0 +1,14 @@
+//() - empty tuple, unit type (void)
+// expression-based language
+fn empty_tuple()/*->()*/{
+    //8 // ;
+}
+//display {}
+//debug print
+fn main(){
+    //세미콜론론
+    let tupl = empty_tuple();
+    //tuple;
+    println("{:?}",tuple);
+
+}
